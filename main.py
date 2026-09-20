596a2f46d6bb7247c4afb6df731bd906e0c866d4
kawediloru <100172103+kawediloru@users.noreply.github.com>
2026-09-20 03:14:40 -0400

    Update main.py
    bug fixes and performance boost (probably)

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ Last Modified 20 September 2026
 # IMPORTS #
 ###########
 
-import discord, glob, numpy as np, os, pyautogui, pygame, sys, yt_dlp
+import asyncio, discord, glob, numpy as np, os, pyautogui, pygame, sys, yt_dlp
 from discord.ext import commands
 from moviepy.editor import VideoFileClip, vfx # moviepy==1.0.3
 from pathlib import Path
-from random import choice
+from queue import Queue, Empty
 from threading import Thread
-from time import sleep
+from uuid import uuid4
 
 
 
@@ -43,7 +43,6 @@ class DiscordReader(commands.Bot):
 
         intents = discord.Intents.default()
         intents.message_content = True
-        intents.reactions = True
         super().__init__(command_prefix='!', intents=intents)
 
         self.add_command(req)
@@ -61,33 +60,45 @@ class DiscordReader(commands.Bot):
 
 # Commands for the bot need global scope. Because fuck you I guess?
 @commands.command()
-async def req(ctx, *, msg):
+async def req(ctx, url):
     '''
     Request command which lets users send video requests in a target channel
     '''
 
-    if ctx.channel.name in DiscordReader.CHANNELS:
+    if ctx.channel.id in DiscordReader.CHANNELS:
         try:
-            vidURL = msg.split(' ')[0]
-            vidLen = YoutubeDownloader.find_video_length(vidURL)
+            vidLen = await asyncio.to_thread(YoutubeDownloader.find_video_length, url)
 
-            if (vidLen <= YoutubeDownloader.MAX_LENGTH):
-                YoutubeDownloader.download_video(vidURL)
-                await ctx.message.add_reaction("✅")
-            else:
+            if vidLen > YoutubeDownloader.MAX_LENGTH:
                 await ctx.send(f"Please keep videos under {YoutubeDownloader.MAX_LENGTH} seconds.", delete_after=5.0)
                 await ctx.message.add_reaction("❌")
+                return
+            
+            await asyncio.to_thread(YoutubeDownloader.download_video, url)
+            await ctx.message.add_reaction("✅")
         
-        except:
-            pass
+        except Exception as e:
+            print(f"Unable to parse message: {e}")
+            await ctx.send("An error occurred!", delete_after=5.0)
+            await ctx.message.add_reaction("⚠️")
     
     else:
         await ctx.send("Please use the assigned mediashare channel.", delete_after=5.0)
 
 
 
+def run_bot():
+    '''
+    Function to run the Discord bot in a thread
+    '''
 
-videoIndex = 0
+    bot = DiscordReader()
+    bot.run(DiscordReader.TOKEN)
+
+
+
+
+
 class YoutubeDownloader:
     '''
     Youtube Downloader functions.
@@ -95,11 +106,12 @@ class YoutubeDownloader:
     and downloading them if they're valid
     '''
 
-    # Settings
+    # customizable rule
     MAX_LENGTH = 60
 
 
 
+    @staticmethod
     def find_video_length(url:str):
         '''
         Get the length of the requested video. Or raise an error if it isn't one
@@ -113,21 +125,20 @@ class YoutubeDownloader:
 
 
 
+    @staticmethod
     def download_video(url:str):
         '''
         Downloads the video
         '''
 
         try:
-            global videoIndex
             global player
 
-            video = yt_dlp.YoutubeDL({"outtmpl": f"video{videoIndex}.%(ext)s"})
+            filename = f"video{uuid4().hex}"
+            video = yt_dlp.YoutubeDL({"outtmpl": f"{filename}.%(ext)s"})
             video.download(url)
 
-            player.fileQueue.append(next(glob.iglob(f"video{videoIndex}.*")))
-
-            videoIndex += 1
+            player.fileQueue.put(next(glob.iglob(f"{filename}.*")))
         
         except Exception as e:
             raise e
@@ -148,10 +159,8 @@ class VideoPlayer:
     SCALE = 1
     VOLUME = 100
 
-    # i suggest not touching these
-    WIDTH, HEIGHT = pyautogui.size()
+    # only for performance
     CQUEUE_SIZE = 5
-    SLEEP_LEN = 1
 
 
 
@@ -162,35 +171,31 @@ class VideoPlayer:
 
         pygame.init()
         self.CLOCK = pygame.time.Clock()
-        if VideoPlayer.FULLSCREEN: self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        else: self.screen = pygame.display.set_mode((VideoPlayer.WIDTH * VideoPlayer.SCALE, VideoPlayer.HEIGHT * VideoPlayer.SCALE))
+        self.WIDTH, self.HEIGHT = pyautogui.size()
 
-        self.fileQueue = []
-        self.clipQueue = []
+        if VideoPlayer.FULLSCREEN: self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        else: self.screen = pygame.display.set_mode((self.WIDTH * VideoPlayer.SCALE, self.HEIGHT * VideoPlayer.SCALE))
+
+        self.fileQueue = Queue()
+        self.clipQueue = Queue(maxsize=VideoPlayer.CQUEUE_SIZE)
 
         self.thrQueueManagement = Thread(target=self.fill_clip_queue, daemon=True)
         self.thrQueueManagement.start()
-
-        self.thrVidManagement = Thread(target=self.manage_videos, daemon=True)
-        self.thrVidManagement.start()
 
 
 
     def fill_clip_queue(self):
         '''
-        Check if the queues can be populated
+        Populate the clip queue
         '''
 
         while True:
-            if len(self.clipQueue) < VideoPlayer.CQUEUE_SIZE:
-                try:
-                    name = self.fileQueue.pop(0)
-                    clip = self.filename_to_video_object(name)
-                    self.clipQueue.append(clip)
-                except:
-                    pass
-            
-            sleep(VideoPlayer.SLEEP_LEN)
+            try:
+                name = self.fileQueue.get()
+                clip = self.filename_to_video_object(name)
+                self.clipQueue.put(clip)
+            except Exception as e:
+                print(f"Failed to append clip: {e}")
 
     
 
@@ -201,26 +206,32 @@ class VideoPlayer:
         '''
 
         try:
-            clip = VideoFileClip(name, target_resolution=(VideoPlayer.HEIGHT * VideoPlayer.SCALE, None)).rotate(90).fx(vfx.mirror_y).volumex(VideoPlayer.VOLUME/100)
+            # Video needs to be rotated 90 degrees and mirrored cuz sure why not.
+            clip = VideoFileClip(name, target_resolution=(self.HEIGHT * VideoPlayer.SCALE, None)).rotate(90).fx(vfx.mirror_y).volumex(VideoPlayer.VOLUME/100)
             return clip
         
         except OSError as e:
-            print(f"{e}; Bad File in List Skipped")
-            return None
+            raise e
 
 
-    
+
+    @staticmethod
     def stop_video(clip):
         '''
         Stop the currently playing clip and signal the next one
         '''
 
-        pygame.mixer.music.stop()
-        pygame.mixer.music.unload()
+        try:
+            pygame.mixer.music.stop()
+            pygame.mixer.music.unload()
+        except pygame.error as e:
+            print(f"Failed to unload music: e")
+        
         clip.close()
 
 
 
+    @staticmethod
     def play_audio(clip):
         '''
         Function to play the audio of a clip when called (through play_video)
@@ -240,11 +251,13 @@ class VideoPlayer:
         
         # if there's audio, play it
         if clip.audio:
-            try: VideoPlayer.play_audio(clip)
-            except Exception as e: print(f"Current audio failed to load: {e}")
+            try:
+                VideoPlayer.play_audio(clip)
+            except Exception as e:
+                print(f"Current audio failed to load: {e}")
         
         try:
-            for frame in clip.iter_frames(fps=clip.fps, dtype="uint8"):
+            for frame in clip.iter_frames(dtype="uint8"):
                 # check for events (also to tick pygame)
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
@@ -257,14 +270,15 @@ class VideoPlayer:
                 
                 # draw it properly
                 frameSurface = pygame.surfarray.make_surface(frame)
-                clipRect = frameSurface.get_rect(center=((VideoPlayer.WIDTH * VideoPlayer.SCALE) // 2, (VideoPlayer.HEIGHT * VideoPlayer.SCALE) // 2))
+                clipRect = frameSurface.get_rect(center=((self.WIDTH * VideoPlayer.SCALE) // 2, (self.HEIGHT * VideoPlayer.SCALE) // 2))
                 self.screen.fill((0, 0, 0))
                 self.screen.blit(frameSurface, clipRect)
                 pygame.display.update()
                 
                 self.CLOCK.tick(clip.fps)
         
-        except OSError as e: print(f"Current video failed to load: {e}")
+        except OSError as e:
+            print(f"Current video failed to load: {e}")
         
         VideoPlayer.stop_video(clip)
 
@@ -276,11 +290,15 @@ class VideoPlayer:
         Basically waits for a video to enter the queue before playing
         '''
         while True:
-            while (len(self.clipQueue) == 0):
-                sleep(VideoPlayer.SLEEP_LEN)
-            
-            self.play_video(self.clipQueue.pop(0))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
 
+            try:
+                clip = self.clipQueue.get_nowait()
+                self.play_video(clip)
+            except:
+                pass
 
 
 
@@ -296,5 +314,8 @@ os.chdir(f"{Path(__file__).resolve().parent}/vids")
 player = VideoPlayer()
 
 # Run the discord bot
-bot = DiscordReader()
-bot.run(DiscordReader.TOKEN)
+botThread = Thread(target=run_bot, daemon=True)
+botThread.start()
+
+# Run the video management loop
+player.manage_videos()
